b9e020687c839f763d723e947e7982fccf1b990d
liao <liao@datamatrix.com>
2024-06-05 20:33:31 +0800

    title

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,21 +1,22 @@
-import './index.scss'
+import scss from './index.module.scss'
 
 export default function Home() {
-    const slogn = ['他山之石', '可以攻玉']
-
     return (
-        <>
-            <div className='slogn-warp'>
-                {slogn.map((word) => {
-                    return (
-                        <div className='word'>
-                            {word.split('').map((chart) => {
-                                return <span className='slogn'>{chart}</span>
-                            })}
-                        </div>
-                    )
-                })}
-            </div>
-        </>
+        <div className={scss.homeContainer}>
+            <section className={scss.content}>
+                <div className={scss.title}>
+                    <span className={scss.text}>个人作品</span>
+                </div>
+                <div className={scss.title}>
+                    <span className={scss.text}>快速文档</span>
+                </div>
+                <div className={scss.title}>
+                    <span className={scss.text}>快速工具</span>
+                </div>
+                <div className={scss.title}>
+                    <span className={scss.text}>奇技淫巧</span>
+                </div>
+            </section>
+        </div>
     )
 }
